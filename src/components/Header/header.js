2b3c66ae876d6cfbd3f52d17f272cd7a827c1ab7
Cole Webster <CWebster_Coding@outlook.com>
2022-12-30 17:40:07 -0500

    Starting to assemble the header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./style.css";
+import Logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <p>Testing - Success</p>
-    </div>
+    <>
+      <div className="Header">
+        <img src={Logo} />
+        <p>Tours</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+    </>
   );
 }
