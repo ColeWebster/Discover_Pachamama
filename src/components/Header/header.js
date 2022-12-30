cd51ecbf767e8fc2948e1a058b8f85eb877e143c
Cole Webster <CWebster_Coding@outlook.com>
2022-12-30 18:41:54 -0500

    Initial style added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,7 +5,17 @@ import Logo from "../../assets/logo.png";
 export default function Header() {
   return (
     <>
-      
+      <header>
+        <img className="logo" src={Logo} alt="Logo" />
+        <nav>
+          <ul className="nav_links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Tours</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
